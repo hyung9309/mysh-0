924d263127526631589fb989b6dba1d51c26b877
hyung9309 <hyung9309@ajou.ac.kr>
2017-10-09 21:43:58 +0900

    three

--- mysh-0/src/utils.c
+++ mysh-0/src/utils.c
@@ -6,40 +6,46 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-	char copy_c[8096];
-	char *insta[200];
+	char *copy_c = NULL;
+	char *insta = NULL;
 
 //	int argc_numb;
 	int arry;
 
 
-	*insta = (char*)malloc(8096);
+	insta = (char*)malloc(8096);
 
-	*argv = (char**)malloc(200);
+	*argv = (char**)malloc(sizeof(char*)*200);
 
 
 
 	strcpy(copy_c, command);
 
 
-	*insta = strtok(copy_c," \t\n");
+	insta = strtok(copy_c," \t\n");
 
 
 	if(insta != NULL)
+	{
 		for(arry = 0; insta != NULL; ++arry)
 		{
 			//*argv[arry] = insta;
+			*argv[arry] = (char*)malloc(sizeof(char)*strlen(insta));
 			strcpy(*argv[arry], insta);
 
-			*insta = strtok(NULL, " \t\n");
+			insta = strtok(NULL, " \t\n");
 
 
 		}
+		
+		*argc = arry+1;
+	}
 	else
-		fprintf(stderr, "why you are here!?\n");
+		*argc = 1;
+//		fprintf(stderr, "why you are here!?\n");
 
 
-	*argc = arry+1; ///the number of arguments.
+//	*argc = arry+1; ///the number of arguments.
 
 
 
