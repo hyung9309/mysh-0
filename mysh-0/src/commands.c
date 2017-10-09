de69cc847f59eaaf7850f01889462733e5725866
hyung9309 <hyung9309@ajou.ac.kr>
2017-10-09 21:54:38 +0900

    four

--- mysh-0/src/commands.c
+++ mysh-0/src/commands.c
@@ -10,11 +10,7 @@
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-
-	char cu2[200];
-
-	if(chdir(argv[1]) != 0)
+	else if(chdir(argv[1]) != 0)
 	return 0;
 
 	else
@@ -22,20 +18,26 @@ int do_cd(int argc, char** argv) {
 }
 
 int do_pwd(int argc, char** argv) {
+char curr[200];
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
 
-	char curr[200];
+//	char curr[200];
 
-	if(getcwd(curr,200) != NULL)
+	else if(getcwd(curr,200) != NULL)
+	{
 //getcwd(curr,200);
+
+
 		printf("%s\n",curr);
+		return 0;
+	}
 	else
 	return -1;
 
 
-	return 0;
+      //return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
